35076666d7bf199f515620a5a6ff2123d8d4481f
StiansbD <46784875+StiansbD@users.noreply.github.com>
2020-12-04 14:36:54 +0100

    add firebase to the project
    npm install --save firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'marie-paule';
+
+  private firebaseConfig = {
+    apiKey: "AIzaSyAJLYAMsSA90ELK3zxZRDVLySyJpUlMRWk",
+    authDomain: "marie-paule-site.firebaseapp.com",
+    projectId: "marie-paule-site",
+    storageBucket: "marie-paule-site.appspot.com",
+    messagingSenderId: "486103791733",
+    appId: "1:486103791733:web:1302c9aa43ff18fcb2a03c",
+    measurementId: "G-XX7E28HRJS"
+  };
+  
+  constructor() {
+    firebase.initializeApp(this.firebaseConfig);
+    firebase.analytics();
+  }
 }
